89818c8e329698cab4b52b6d05598672d5df8f2e
hotsixisbest <babytaikoer>
2023-12-13 16:21:54 +0900

    deleteUser 함수 추가

---
+++ src/deleteUser.ts
@@ -0,0 +1,20 @@
+import { runQuery } from "@sveltekit-board/db";
+import { UserError } from "./error";
+
+export async function deleteUser(id:string){
+    try{
+        let r = await runQuery(async(run) => {
+            return await run("DELETE FROM `user` WHERE `id` = ?", [id]);
+        })
+        
+        if(r.affectedRows == 0){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+    catch(err){
+        throw new UserError("DATABASE_ERROR",undefined, err)
+    }
+}
