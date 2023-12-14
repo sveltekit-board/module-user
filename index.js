83d5439b6a610ded4b8558ae2181051b7634c315
hotsixisbest <babytaikoer>
2023-12-14 09:19:08 +0900

    Ver. 0.1.2

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const alterUserData_1 = __importDefault(require("./src/alterUserData"));
+const createUser_1 = require("./src/createUser");
+const deleteUser_1 = require("./src/deleteUser");
+const getUser_1 = __importDefault(require("./src/getUser"));
+const user = {
+    alterUserData: alterUserData_1.default,
+    createUser: createUser_1.createUser,
+    deleteUser: deleteUser_1.deleteUser,
+    getUser: getUser_1.default
+};
+exports.default = user;
